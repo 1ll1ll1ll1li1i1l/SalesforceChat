f663840b64fbc8506f05152c3c2889125c0aa7ce
정혁 <wwjeel6@naver.com>
2024-07-18 16:46:06 +0900

    junghyuck --add
    깃허브 초기 환경 셋팅

---
+++ src/main/java/SalesforceGPT/middlewareServer/JunghyuckApplication.java
@@ -0,0 +1,13 @@
+package SalesforceGPT.middlewareServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunghyuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunghyuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SalesforceGPT/middlewareServer/JunghyuckApplicationTests.java
@@ -0,0 +1,13 @@
+package SalesforceGPT.middlewareServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunghyuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
